cd15cb803d7aa1b007bb778fffde64c65c32f39c
yuuto <yuuttoo@gmail.com>
2019-08-30 00:11:02 +0800

    設置webpack, babel環境/串接外部API/使用promise語法撰寫search功能

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+import Search from './models/Search';
+
+/** Global state of the APP
+ *  -Search Object
+ *  -Current recipe object
+ *  -Shopping List object
+ *  -Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //TODO
+
+    if(query) {
+        //2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault(); 
+    controlSearch();
+
+});
+
+const search = new Search('pizza');
+console.log(search);
+
