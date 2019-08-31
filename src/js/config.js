9f5f5c1f959b500cb314129732180dc283eace85
yuuto <yuuttoo@gmail.com>
2019-08-31 17:01:36 +0800

    新增食譜介面、原料單位計算功能

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'bd24ca9d58a95553aff835aad08e69f8';
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
 
 /** Global state of the APP
  *  -Search Object
@@ -8,30 +13,98 @@ import Search from './models/Search';
  */
 const state = {};
 
+
+/**Search Controller  */
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = 'pizza' //TODO
+    const query = searchView.getInput(); //TODO
+    
+   
 
     if(query) {
         //2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput(); 
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+    try {
+         // 4) Search for recipes
+    
+         await state.search.getResults();
 
-        // 4) Search for recipes
-        await state.search.getResults();
-
-        // 5) Render results on UI
-        console.log(state.search.result);
+         // 5) Render results on UI
+         clearLoader();
+         searchView.renderResults(state.search.result);
+     
+    } catch (err) {
+        alert('Something wrong with the search...')
+        clearLoader();
     }
+             
+    }
+
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
-    e.preventDefault(); 
-    controlSearch();
 
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
 });
 
-const search = new Search('pizza');
-console.log(search);
 
+
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+ /** Recipe Controller  */
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if(id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+
+
+     try {
+        //Get recipe data and parse ingredients
+        await state.recipe.getRecipe();
+        console.log(state.recipe.ingredients);
+        state.recipe.parseIngredients();
+
+        //Calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+
+        // Render recipe
+       clearLoader();
+       recipeView.renderRecipe(state.recipe);
+
+     } catch (error) {
+         console.log(error);
+         alert('Error processing recipe');
+     }
+       
+    }
+
+};
+ 
+
+
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+//change to one line
+['hashchange', 'load'].forEach(event => window.addEventListener(event,controlRecipe));
